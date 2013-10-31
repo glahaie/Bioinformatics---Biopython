9e550b07778a96b0dd569a2f50e525131cb5a288
Guillaume Lahaie <glahaie@gmail.com>
2013-10-30 23:34:07 -0400

    Pas de commti de la journée, c'est le temps de le faire

--- devoir1/blast_parser.py
+++ devoir1/blast_parser.py
@@ -2,27 +2,41 @@
 
 
 #Parser pour un fichier XML de résultat blast
+#Spécifique à la question 2 du devoir. Je sais
+#ici que chaque hit a seulement un hsp
 
+import sys
+import os
 from Bio.Blast import NCBIXML
-
-
-nom_fichier = "blast_contig_132.xml"
-result_handle = open(nom_fichier)
-
-blast_record = NCBIXML.read(result_handle)
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio import Entrez
 
 #On choisit une E-VALUE
 E_VALUE_THRESH = 0.04
+Entrez.email = "glahaie@gmail.com"
+path = "annexes/question_2/"
 
-for alignment in blast_record.alignments:
-     for hsp in alignment.hsps:
-         if hsp.expect < E_VALUE_THRESH:
-             print '****Alignment****'
-             print 'sequence:', alignment.title
-             print 'length:', alignment.length
-             print 'e value:', hsp.expect
-             print 'total score:',hsp.score
-             print hsp.query[0:75] + '...'
-             print hsp.match[0:75] + '...'
-             print hsp.sbjct[0:75] + '...'
+path_fichier = path + "blast_contig_"+sys.argv[1] + ".xml"
+
+with open(path_fichier) as fichier:
+    blast_record = NCBIXML.read(fichier)
+    i = 0
+    path_result = path + "contig_"+sys.argv[1]+"/"
+    if not os.path.exists(path_result):
+        os.makedirs(path_result)
+    for alignment in blast_record.alignments:
+        for hsp in alignment.hsps:
+            if hsp.expect < E_VALUE_THRESH:
+#On obtient alors le fichier genbank
+                handle = Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", id=alignment.accession, seq_start=hsp.sbjct_start, seq_stop=hsp.sbjct_end)
+                seq_record= SeqIO.read(handle, "gb")
+                handle.close()
+                nom_fichier = path_result + alignment.accession + ".gb"
+                SeqIO.write(seq_record, nom_fichier, "gb")
+
+        i += 1
+        if i > 10:
+            break
+
 
